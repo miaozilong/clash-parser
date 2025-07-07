c560e9720c1c38705dd68f05cdfa6dd1c14716e4
miaozilong <miaozilong@gmail.com>
2025-07-07 21:47:18 +0800

    香港负载均衡只适用ss节点

--- ikuuu.js
+++ ikuuu.js
@@ -75,7 +75,9 @@ module.exports.parse = async (raw, {axios, yaml, notify, console}, {name, url, i
     // 给各个负载均衡组添加匹配代理
     obj['proxy-groups'].forEach(group => {
         if (group.name.includes('香港')) {
-            group.proxies = proxyNames.filter(name => name.includes('香港'));
+            group.proxies = obj.proxies
+                .filter(proxy => proxy.name.includes('香港') && proxy.type === 'ss')
+                .map(proxy => proxy.name);
         } else if (group.name.includes('下载专用')) {
             group.proxies = proxyNames.filter(name => name.includes('x0.01'));
         } else if (group.name.includes('所有日本')) {
